a20576a29710a20ce98b450e5a11ebe7f281c09c
Jon White <jwhite@seriflabs.com>
2018-06-07 14:18:02 +0100

    GMT::App

--- Main.cpp
+++ Main.cpp
@@ -1,11 +1,15 @@
 #include "PanelView.h"
 #include "Window.h"
 
+#include "gmt/App.h"
+
 #include <SFML/Graphics.hpp>
 #include <SFML/OpenGL.hpp>
 
 int main()
 {
+	GMT::App app;
+
 	Window window;
 	window.setPosition({30, 50});
 	window.setSize({ 1920 * 4 / 5 + 300, 1080 * 4 / 5 }); // TODO: Get from Window.
