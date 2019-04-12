43520b71102732106427d16c87ce8b4814daecc4
Jon White <jxxwhite@gmail.com>
2019-04-12 13:56:23 +0100

    wip

--- PanelView.cpp
+++ PanelView.cpp
@@ -5,6 +5,33 @@
 
 #include <iostream>
 
+class Container : public Control
+{
+public:
+
+protected:
+	virtual void UpdateLayout(const sf::FloatRect logRect) override;
+
+private:
+};
+
+void Container::UpdateLayout(const sf::FloatRect logRect)
+{
+	const int height = 30;
+	const int gap = 10;
+
+	sf::IntRect rect(gap, gap, (int)logRect.width - gap * 2, height);
+
+	for (auto& control : m_children)
+	{
+		control->SetRect(rect);
+		rect.top += height + gap;
+	}
+}
+
+
+
+
 PanelView::PanelView()
 {
 	//m_children.push_back(std::make_unique<Button>());
@@ -20,14 +47,5 @@ void PanelView::Draw(sf::RenderWindow& window) const
 
 void PanelView::UpdateLayout(const sf::FloatRect logRect)
 {
-	const int height = 30;
-	const int gap = 10;
 
-	sf::IntRect rect(gap, gap, (int)logRect.width - gap * 2, height);
-
-	for (auto& control : m_children)
-	{
-		control->SetRect(rect);
-		rect.top += height + gap;
-	}
 }
